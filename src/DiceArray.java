c72db1ea2283f209fbf9d96fce6f9271c9b18bb5
Philip <Philip@philips-mbp.fios-router.home>
2018-10-28 18:22:23 -0400

    Wow

--- src/DiceArray.java
+++ src/DiceArray.java
@@ -5,6 +5,8 @@
  * Period 7
  *
  */
+
+import java.util.Arrays;
 public class DiceArray 
 {
 	public static void main(String[] args) 
@@ -12,12 +14,14 @@ public class DiceArray
 		Dice die1 = new Dice();
 		Dice die2 = new Dice();
 		int[] count = new int[13];
-		
-		for(int i=0; i<count.length; i++)
+//Roll dice 1000 times
+		for(int i=0; i<1000; i++)
 		{
 			int roll = die1.roll() + die2.roll();
+			count[roll] += 1;
 		}
-
+//Print array using toString and Arrays class
+		System.out.print(Arrays.toString(count));
 	}
 
 }
